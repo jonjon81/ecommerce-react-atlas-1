81b73d81e366867e7f2300d36f164cffd036810a
Jon Atlas <jjjatlas@hotmail.com>
2023-10-09 16:59:40 +0100

    Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,18 +4,91 @@ import ScrollButton from './ScrollButton';
 const Footer = () => {
   return (
     <Container>
-      <h5>
-        &copy; {new Date().getFullYear()}
-        <span>&nbsp;Atlas Ecommerce</span>
-      </h5>
-      <h5>&nbsp;All rights reserved</h5>
+      <div className="footer-container">
+        <ul>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">lorem</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="/test">Lorem, ipsum</a>
+          </li>
+          <li>
+            <a href="/test">Lorem</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">Lorem, ipsum dolor</a>
+          </li>
+          <li>
+            <a href="/test">lorem</a>
+          </li>
+        </ul>
+      </div>
+      <div className="rights-reserved">
+        <h5>
+          &copy; {new Date().getFullYear()}
+          <span>&nbsp;Atlas Ecommerce</span>
+        </h5>
+        <h5>&nbsp;All rights reserved</h5>
+      </div>
       <ScrollButton />
     </Container>
   );
 };
 
 const Container = styled.footer`
-  height: 5rem;
+  height: auto;
+  padding: 2rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -33,8 +106,27 @@ const Container = styled.footer`
     text-transform: none;
     line-height: 1.25;
   }
+
+  .footer-container {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    max-width: 1170px;
+    padding-bottom: 3rem;
+    ul {
+      text-align: left;
+    }
+}
+    a {
+      color: #fff;
+      
+      &:hover {
+        text-decoration:underline;
+      }
+    }
+  }
   @media (min-width: 776px) {
-    flex-direction: row;
+    flex-direction: column;
   }
 `;
 
