0cf0ab8a0df32016403f81f04a0ffc1c87fc64ce
Jon Atlas <jjjatlas@hotmail.com>
2023-10-12 23:55:05 +0100

    fix error

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FaSistrix } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 const scrollTop = () => {
