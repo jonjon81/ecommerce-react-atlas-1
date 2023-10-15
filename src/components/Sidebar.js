ab0514067fe1fd04315004ecf50b86a4816050ad
Jon Atlas <jjjatlas@hotmail.com>
2023-10-15 20:46:19 +0100

    update side menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
-import { FaTimes, FaChevronRight } from 'react-icons/fa';
+import { FaTimes, FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import { links } from '../utils/constants';
 import { ProductSublinksAz } from '../utils/constants';
 import styled from 'styled-components';
@@ -11,6 +11,12 @@ import { useUserContext } from '../context/user_context';
 import { useFilterContext } from '../context/filter_context';
 
 const Sidebar = () => {
+  const [isActive, setActive] = useState('false');
+
+  const handleSubmenuToggle = () => {
+    setActive(!isActive);
+  };
+
   const { isSidebarOpen, closeSidebar } = useProductsContext();
   const { myUser } = useUserContext();
   const { clearFilters } = useFilterContext();
@@ -34,14 +40,17 @@ const Sidebar = () => {
             if (text === 'products') {
               return (
                 <li key={id}>
-                  <Link onClick={clearFilters}>
+                  <Link onClick={handleSubmenuToggle}>
                     {text} <FaChevronRight className="chevron-right" />
                   </Link>
-                  <ul className="sublinks">
+                  <ul className={`sublinks ${isActive ? null : 'active'}`}>
+                    <button onClick={handleSubmenuToggle} className="back-to-main">
+                      <FaChevronLeft /> <span>back to main menu</span>
+                    </button>
                     {ProductSublinksAz.map((sublink) => {
                       return (
                         <li key={sublink.id}>
-                          <Link onClick={clearFilters} to={sublink.url}>
+                          <Link onClick={doubleFunction} to={sublink.url}>
                             {sublink.text}
                           </Link>
                         </li>
@@ -81,6 +90,16 @@ const SidebarContainer = styled.div`
     align-items: center;
     padding: 1rem 1.5rem;
     color: #fff;
+  }
+  .back-to-main {
+    padding: 1rem;
+    position: absolute;
+    top: 0;
+    display: flex;
+    cursor: pointer;
+    svg {
+      margin-right: 5px;
+    }
   }
   .close-btn {
     font-size: 2rem;
@@ -145,14 +164,14 @@ const SidebarContainer = styled.div`
     position: absolute;
     top: 5rem;
     display: none;
+    padding-top: 50px;
     flex-wrap: wrap;
+    &.active {
+      display: flex;
+    }
     li {
       width: 50%;
     }
-  }
-
-  ul.links li:hover > ul {
-    display: flex;
   }
 
   svg.chevron-right {
